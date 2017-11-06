c874a37341d1139da327457a89a57080a3732c35
Marinic <Marinic@DESKTOP-LUPKT5O>
2017-11-06 20:16:12 +0100

    inicijalni upload

---
+++ MailSender/ConsoleAppMail/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleAppMail;
+
+namespace ConsoleAppMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailClient mail = new MailClient();
+            mail.SendEmail("domagoj.persic@vsmti.hr", "Proba", "Ovo nije u redu od tebe");
+            Console.ReadKey();
+        }
+    }
+}
